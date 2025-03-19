c72850a7761f75762b9f63cbf9244300760510df
Claus-Justus Heine <himself@claus-justus-heine.de>
2025-03-19 23:09:45 +0100

    Fix PHP warnings.

--- lib/plugins/authldap/auth.php
+++ lib/plugins/authldap/auth.php
@@ -243,8 +243,8 @@ class auth_plugin_authldap extends AuthPlugin
         // general user info
         $info['dn'] = $user_result['dn'];
         $info['gid'] = $user_result['gidnumber'][0] ?? null;
-        $info['mail'] = $user_result['mail'][0];
-        $info['name'] = $user_result['cn'][0];
+        $info['mail'] = $user_result['mail'][0] ?? null;
+        $info['name'] = $user_result['cn'][0] ?? null;
         $info['grps'] = [];
 
         // overwrite if other attribs are specified.
